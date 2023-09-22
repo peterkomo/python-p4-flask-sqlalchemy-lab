6c8505a12b371ff521931860a3fc208783784114
peterkomo <mwangipeterkomo@gmail.com>
2023-09-23 01:27:49 +0300

    complited assigment

--- server/app.py
+++ server/app.py
@@ -19,16 +19,54 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        response_body = f"""
+            <ul>ID: {animal.id}</ul>
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+        """
+        return make_response(response_body, 200)
+    else:
+        return make_response("Animal not found", 404)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        animal_names_html = ""
+        for animal in zookeeper.animals:
+            animal_names_html += f"<ul>Animal: {animal.name}</ul>"
+
+        response_body = f"""
+            <ul>ID: {zookeeper.id}</ul>
+            <ul>Name: {zookeeper.name}</ul>
+            <ul>Birthday: {zookeeper.birthday}</ul>
+            {animal_names_html}
+        """
+        return make_response(response_body, 200)
+    else:
+        return make_response("Zookeeper not found", 404)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        animal_names_html = ""
+        for animal in enclosure.animals:
+            animal_names_html += f"<ul>Animal: {animal.name}</ul>"
 
+        response_body = f"""
+            <ul>ID: {enclosure.id}</ul>
+            <ul>Environment: {enclosure.environment}</ul>
+            <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+            {animal_names_html}
+        """
+        return make_response(response_body, 200)
+    else:
+        return make_response("Enclosure not found", 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -8,16 +8,31 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
-
+    __tablename__= 'zookeepers'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birthday = db.Column(db.String(20), nullable=True)
+    
+    # Modify the backref name to 'caretaker' instead of 'zookeeper'
+    caretaker = db.relationship('Animal', backref=db.backref('caretaker', lazy=True))
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
-
     id = db.Column(db.Integer, primary_key=True)
-
+    environment = db.Column(db.String(20), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    
+    # Modify the backref name to 'habitat'
+    animals = db.relationship('Animal', backref=db.backref('habitat', lazy=True))
 class Animal(db.Model):
-    __tablename__ = 'animals'
-
+    __tablename__= 'animals'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+    
+    # Modify the backref name to 'animal_enclosure'
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'), nullable=True)
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'), nullable=True)
+    
+    zookeeper = db.relationship('Zookeeper', backref=db.backref('animals', lazy=True))
+    enclosure = db.relationship('Enclosure', backref=db.backref('animal_enclosure', lazy=True))
